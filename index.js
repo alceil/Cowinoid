552a697f80213040a53aab29db1cb6abbb6ad23d
alceil <ashishshajisrampickal@gmail.com>
2021-05-17 02:01:17 +0530

    first commit

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-require("dotenv").config();
-
-const { Client } = require('discord.js');
-const client = new Client();
-client.login(process.env.TOKEN);
-
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
 
 
 
-// require('dotenv').config();
-// const Discord = require('discord.js');
-// const bot = new Discord.Client();
-// const TOKEN = process.env.TOKEN;
 
-// bot.login(TOKEN);
+try {
+    bot.login(TOKEN);
+} catch (error) {
+    console.log(error);
+}
 
-// bot.on('ready', () => {
-//   console.info(`Logged in as ${bot.user.tag}!`);
-// });
 
-// bot.on('message', msg => {
-//   if (msg.content === 'ping') {
-//     msg.reply('pong');
-//     msg.channel.send('pong');
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
 
-//   } else if (msg.content.startsWith('!kick')) {
-//     if (msg.mentions.users.size) {
-//       const taggedUser = msg.mentions.users.first();
-//       msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-//     } else {
-//       msg.reply('Please tag a valid user!');
-//     }
-//   }
-// });
+bot.on('message', msg => {
+  if (msg.content === 'kindi') {
+    msg.reply('yo enn kindi 🎃');
+    msg.channel.send('dear kindi 🎃');
+
+  } else if (msg.content.startsWith('!kick')) {
+    if (msg.mentions.users.size) {
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    } else {
+      msg.reply('Please tag a valid user!');
+    }
+  }
+});
